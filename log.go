574e251f402b1b890086a23495b09a77e27bf73e
lizw <zwli@dataman-inc.com>
2016-06-13 10:45:21 +0800

    add example

---
+++ log.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+/*
+
+	log使用示例
+	1、统一采用 github.com/Sirupsen/logrus
+	2、该log有六种级别
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+	// Calls os.Exit(1) after logging
+	log.Fatal("Bye.")
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+
+	Fatal和Panic会退出程序 不建议使用
+
+	3、发生错误需要用log.Error打印错误信息
+	   正常流程使用log.Infoln打印，不建议打印太多
+	      如果加用于调试的信息 加DEBUG log.Debugln打印
+
+*/
+
+func get_info() string {
+
+	return "iiiiii"
+}
+
+func do_something() error {
+	i := get_info()
+
+	/* 如果需要对信息进行记录 可以用log.Debug*/
+
+	log.Debugln(i)
+
+	return nil
+}
+
+func Test() {
+
+	err := do_something()
+
+	/*发生错误需要用log.Error打印错误信息*/
+	if err != nil {
+		log.Errorln(err.Error())
+	}
+
+	/*可以根据情况进行正常流程打印*/
+	log.Infoln("do_something successfully")
+
+}
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"os"
+)
+
+/* 如果不同模块需要配置各自的mysql 前面加上模块名 方便运维配置*/
+type EnvConfig struct {
+	Env1           string
+	Env2           string
+	A_MYSQL_PASSWD string
+}
+
+var Env EnvConfig
+
+/* 缺少环境变量 程序退出 使用log.Fatalf打印*/
+func exitMissingEnv(env string) {
+	log.Fatalf("exit missing env %s", env)
+}
+
+func initEnv() {
+	Env1 := os.Getenv("ENV1")
+	if Env1 == "" {
+		exitMissingEnv("ENV1")
+	}
+
+	Env2 := os.Getenv("ENV2")
+	if Env2 == "" {
+		exitMissingEnv("ENV2")
+	}
+
+	A_MYSQL_PASSWD := os.Getenv("A_MYSQL_PASSWD")
+	if A_MYSQL_PASSWD == "" {
+		exitMissingEnv("A_MYSQL_PASSWD")
+	}
+
+	Env.Env1 = Env1
+	Env.Env2 = Env2
+	Env.A_MYSQL_PASSWD = A_MYSQL_PASSWD
+}
+
+func main() {
+	initEnv()
+	/*使用log示例*/
+	Test()
+	fmt.Println("birth cry")
+
+}
